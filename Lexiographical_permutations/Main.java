97f4fbb989018a3892d828086c6e79042000d190
Jimmy <jimjacob29@gmail.com>
2020-08-05 11:44:43 +0530

    corrected code

--- Lexiographical_permutations/Main.java
+++ Lexiographical_permutations/Main.java
@@ -6,26 +6,42 @@ public class Main
     {
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        if(size%3==2)
+        if(size>4)
         {
-            System.out.print(size+" ");
-            size--;
+            for(int i=1;i<=size;i++)
+            {
+                if(i==size)
+                {
+                    System.out.print((size-3)+" ");
+                }
+                else if(i==(size-3))
+                {
+                    System.out.print(size+" ");
+                }
+                else
+                {
+                    System.out.print(i+" ");
+                }
+            }
         }
-        for(int i=0;i<=size;i=i+3)
+        else if(size<=4)
         {
-            if(i+1<=size)
+            for (int i = 0; i <= size; i = i + 3)
             {
-                System.out.print((i+1)+" ");
+                if (i + 1 <= size)
+                {
+                    System.out.print((i + 1) + " ");
+                }
+                if (i + 3 <= size)
+                {
+                    System.out.print((i + 3) + " ");
+                }
+                if (i + 2 <= size)
+                {
+                    System.out.print((i + 2) + " ");
+                }
+                else break;
             }
-            if(i+3<=size)
-            {
-                System.out.print((i+3)+" ");
-            }
-            if(i+2<=size)
-            {
-                System.out.print((i +2)+" ");
-            }
-            else break;
         }
     }
 }
